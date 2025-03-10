5ff385acc8a757f09e8525baf5c3bd4930c73bff
nandah4 <nyustira6@gmail.com>
2025-03-10 16:24:29 +0700

    feat: add news card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:my_flutter1/data/data_article.dart';
 import 'package:my_flutter1/themes/group_colors.dart';
 import 'package:my_flutter1/widgets/appbar.dart';
@@ -12,185 +13,166 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final colors = Theme.of(context).extension<ColorsGroup>();
     return Scaffold(
-      appBar: AppBarCustom(title: "Halo."),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: dataArticle.map((datas) {
-                  return SizedBox(
-                    height: 250,
-                    width: MediaQuery.of(context).size.width,
-                    child: Stack(
-                      children: [
-                        Image.asset(
-                          'assets/images/hero-slide1.jpg',
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                        Container(
-                            width: double.infinity,
-                            height: double.infinity,
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                  colors: [
-                                    Colors.transparent,
-                                    Colors.black.withValues(alpha: 0.6),
-                                  ],
-                                  begin: Alignment.topCenter,
-                                  end: Alignment.bottomCenter),
-                            )),
-                        Positioned(
-                            top: 16,
-                            left: 16,
-                            child: Container(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 7, vertical: 3),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(25)),
-                              child: Text(
-                                '🔥 News Update',
-                                style: TextStyle(
-                                    fontSize: 12, fontWeight: FontWeight.w400),
+        appBar: AppBarCustom(title: "Halo."),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Column(
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: dataArticle.map((datas) {
+                        return SizedBox(
+                          height: 250,
+                          width: MediaQuery.of(context).size.width,
+                          child: Stack(
+                            children: [
+                              Image.asset(
+                                'assets/images/hero-slide1.jpg',
+                                width: double.infinity,
+                                fit: BoxFit.cover,
                               ),
-                            )),
-                        Positioned(
-                            bottom: 16,
-                            left: 16,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(50),
-                                      child: Image.asset(
-                                        'assets/images/hero-slide1.jpg',
-                                        height: 25,
-                                        width: 25,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                    SizedBox(width: 10),
-                                    Text(
-                                      'By ${datas['author']} - ${datas['times']} Min',
+                              Container(
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        colors: [
+                                          Colors.transparent,
+                                          Colors.black.withValues(alpha: 0.6),
+                                        ],
+                                        begin: Alignment.topCenter,
+                                        end: Alignment.bottomCenter),
+                                  )),
+                              Positioned(
+                                  top: 16,
+                                  left: 16,
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 7, vertical: 3),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius:
+                                            BorderRadius.circular(25)),
+                                    child: Text(
+                                      '🔥 News Update',
                                       style: TextStyle(
-                                          fontSize: 11,
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w500),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400),
                                     ),
-                                  ],
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(top: 8),
-                                  width: 320,
-                                  child: Text(
-                                    '${datas['title']}',
-                                    style: TextStyle(
-                                        fontSize: 19,
-                                        fontWeight: FontWeight.w500,
-                                        color: Colors.white),
-                                  ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(top: 13),
-                                  child: Row(children: [
-                                    Container(
-                                        margin: EdgeInsets.only(right: 5),
-                                        height: 2.5,
-                                        width: 25,
-                                        color: colors?.backgroundCategory
-                                            ?.withValues(alpha: 0.5)),
-                                    Container(
-                                        margin: EdgeInsets.only(right: 5),
-                                        height: 2.5,
-                                        width: 25,
-                                        color: colors?.backgroundCategory
-                                            ?.withValues(alpha: 0.9))
-                                  ]),
-                                )
-                              ],
-                            )),
-                      ],
-                    ),
-                  );
-                }).toList()),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 25),
-            height: 33,
-            width: MediaQuery.of(context).size.width,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: scrollMenu.length,
-                itemBuilder: (context, item) {
-                  return Padding(
-                    padding: EdgeInsets.only(
-                        left: item == 0 ? 16 : 5,
-                        right: item == scrollMenu.length - 1 ? 16 : 5),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color:
-                              item == 0 ? Colors.green[800] : Colors.grey[200],
-                          borderRadius: BorderRadius.circular(18)),
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${scrollMenu[item]}',
-                        style: TextStyle(
-                          color: item == 0 ? Colors.white : Colors.grey[700],
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-          ),
-          Container(
-            child: ListView.builder(itemBuilder: (context, item) {}),
-          )
-
-          // SizedBox(
-          //     width: MediaQuery.of(context).size.width,
-          //     height: MediaQuery.of(context).size.height,
-          //     child: Expanded(
-          //       child: ListView.builder(
-          //           scrollDirection: Axis.vertical,
-          //           shrinkWrap: true,
-          //           physics: NeverScrollableScrollPhysics(),
-          //           itemCount: 2,
-          //           itemBuilder: (context, item) {
-          //             return CustomListTile(
-          //                 image: 'assets/images/hero-slide1.jpg',
-          //                 imageAuthor: 'assets/images/hero-slide1.jpg',
-          //                 nameAuthor: 'John Doe',
-          //                 title: 'Lorem Ipsum',
-          //                 category: 'category',
-          //                 hour: 1,
-          //                 icon: Icon(Icons.ac_unit));
-          //             // return ListTile(
-          //             //   minTileHeight: 100,
-          //             //   leading: ClipRRect(
-          //             //     borderRadius: BorderRadius.circular(10),
-          //             //     child: Image.asset(
-          //             //       'assets/images/hero-slide1.jpg',
-          //             //       height: 100,
-          //             //       width: 100,
-          //             //       fit: BoxFit.cover,
-          //             //     ),
-          //             //   ),
-          //             //   title: Text("Item ke - ${item + 1}"),
-          //             //   subtitle: Text("Lorem ipsum dolor sit amet!"),
-          //             //   trailing: Icon(Icons.save_as_rounded),
-          //             // );
-          //           }),
-          //     )),
-        ],
-      ),
-    );
+                                  )),
+                              Positioned(
+                                  bottom: 16,
+                                  left: 16,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(50),
+                                            child: Image.asset(
+                                              'assets/images/hero-slide1.jpg',
+                                              height: 25,
+                                              width: 25,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          SizedBox(width: 10),
+                                          Text(
+                                            'By ${datas['author']} - ${datas['times']} Min',
+                                            style: TextStyle(
+                                                fontSize: 11,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                        ],
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(top: 8),
+                                        width: 320,
+                                        child: Text(
+                                          '${datas['title']}',
+                                          style: TextStyle(
+                                              fontSize: 19,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(top: 13),
+                                        child: Row(children: [
+                                          Container(
+                                              margin: EdgeInsets.only(right: 5),
+                                              height: 2.5,
+                                              width: 25,
+                                              color: colors?.backgroundCategory
+                                                  ?.withValues(alpha: 0.5)),
+                                          Container(
+                                              margin: EdgeInsets.only(right: 5),
+                                              height: 2.5,
+                                              width: 25,
+                                              color: colors?.backgroundCategory
+                                                  ?.withValues(alpha: 0.9))
+                                        ]),
+                                      )
+                                    ],
+                                  )),
+                            ],
+                          ),
+                        );
+                      }).toList()),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 25, bottom: 25),
+                  height: 33,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: scrollMenu.length,
+                      itemBuilder: (context, item) {
+                        return Padding(
+                          padding: EdgeInsets.only(
+                              left: item == 0 ? 16 : 5,
+                              right: item == scrollMenu.length - 1 ? 16 : 5),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: item == 0
+                                    ? Colors.green[800]
+                                    : Colors.grey[200],
+                                borderRadius: BorderRadius.circular(18)),
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            alignment: Alignment.center,
+                            child: Text(
+                              '${scrollMenu[item]}',
+                              style: TextStyle(
+                                color:
+                                    item == 0 ? Colors.white : Colors.grey[700],
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    children: [
+                      NewsCard(colors: colors),
+                      NewsCard(colors: colors),
+                      NewsCard(colors: colors),
+                      NewsCard(colors: colors)
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
--- lib/widgets/card_list_tile.dart
+++ lib/widgets/card_list_tile.dart
@@ -1,46 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:my_flutter1/themes/group_colors.dart';
+// import 'package:flutter/widgets.dart';
 
-class CustomListTile extends StatefulWidget {
-  final String image;
-  final String imageAuthor;
-  final String nameAuthor;
-  final String title;
-  final String category;
-  final int hour;
-  final Widget icon;
+// class CustomListTile extends StatefulWidget {
+//   final String image;
+//   final String imageAuthor;
+//   final String nameAuthor;
+//   final String title;
+//   final String category;
+//   final int hour;
+//   final Widget icon;
 
-  const CustomListTile({
+//   const CustomListTile({
+//     super.key,
+//     required this.image,
+//     required this.imageAuthor,
+//     required this.nameAuthor,
+//     required this.title,
+//     required this.category,
+//     required this.hour,
+//     required this.icon,
+//   });
+
+//   @override
+//   State<StatefulWidget> createState() => _CustomListTile();
+// }
+
+class NewsCard extends StatelessWidget {
+  const NewsCard({
     super.key,
-    required this.image,
-    required this.imageAuthor,
-    required this.nameAuthor,
-    required this.title,
-    required this.category,
-    required this.hour,
-    required this.icon,
+    required this.colors,
   });
 
-  @override
-  State<StatefulWidget> createState() => _CustomListTile();
-}
+  final ColorsGroup? colors;
 
-class _CustomListTile extends State<CustomListTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 100,
+      margin: EdgeInsets.only(top: 15),
+      width: MediaQuery.of(context).size.width,
+      height: 120,
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(
+                  width: 1.5,
+                  color: colors?.backgroundCategory ??
+                      Colors.grey.withValues(alpha: 1.2)))),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset(
-              widget.image,
-              height: 100,
-              width: 100,
+          Padding(
+            padding: EdgeInsets.only(right: 15),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(7),
+              child: Image.asset(
+                'assets/images/hero-slide1.jpg',
+                height: 85,
+                width: 85,
+                fit: BoxFit.cover,
+              ),
             ),
-          )
+          ),
+          Container(
+            width: 240,
+            // decoration:
+            //     BoxDecoration(border: Border.all(width: 1)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          'assets/images/hero-slide1.jpg',
+                          height: 25,
+                          width: 25,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Maureen O'Hare",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.underline),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "The World's biggest Cruise Ship is Almost Ready.",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Travel",
+                        style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "4 Hours Ago",
+                        style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "3 Min",
+                        style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+              child: Align(
+            alignment: Alignment.topRight,
+            child: Image.asset(
+              'assets/icons/bookmark-icon.png',
+              color: Colors.grey[800],
+              width: 20,
+              height: 20,
+            ),
+          ))
         ],
       ),
     );
